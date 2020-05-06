38c760b1b0dcae84a9d0307658cf174cf505384f
Steve Poll <stevepoll@gmail.com>
2020-05-06 16:54:15 -0400

    Updates

---
+++ precache-manifest.c11bb50660a9dfb113ee52e83dca519f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33e87dc86a080786c569d3c93b7166fb",
+    "url": "/simple-todo/index.html"
+  },
+  {
+    "revision": "9ebf65f546e4142c0efa",
+    "url": "/simple-todo/static/css/main.cf6e75da.chunk.css"
+  },
+  {
+    "revision": "d018f18d7bb2271b215a",
+    "url": "/simple-todo/static/js/2.5f1c32b5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-todo/static/js/2.5f1c32b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ebf65f546e4142c0efa",
+    "url": "/simple-todo/static/js/main.989dfa60.chunk.js"
+  },
+  {
+    "revision": "ca24f8bbb360c3af9ec0",
+    "url": "/simple-todo/static/js/runtime-main.e40cf469.js"
+  }
+]);
